f5728850ab504f0cc5bf82b3584fa722112807c6
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-06 13:36:23 -0400

    Initial content display for event details

---
+++ app/routes/_content.events.$event_id/route.tsx
@@ -0,0 +1,44 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import createDBClient from "~/utils/supabase/server";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  params.event_id;
+  const dbClient = createDBClient();
+  const {
+    data: event,
+    error,
+    status,
+    statusText,
+  } = await dbClient.from("events").select("*").eq("id", params.event_id!);
+  return { event: event?.[0], error, status, statusText };
+}
+
+function ViewEvent() {
+  const { event } = useLoaderData<typeof loader>();
+  return (
+    <section className="">
+      <div className="gap-16 items-start lg:grid lg:grid-cols-2">
+        <div className="font-light sm:text-lg">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold">
+            {event?.name}
+          </h2>
+          <p className="mb-4">{event?.description}</p>
+          <p>
+            We are strategists, designers and developers. Innovators and problem
+            solvers. Small enough to be simple and quick.
+          </p>
+        </div>
+        <div className="">
+          <img
+            className="w-full rounded-lg"
+            src={event?.img_url || ""}
+            alt={event?.name || "event"}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ViewEvent;
