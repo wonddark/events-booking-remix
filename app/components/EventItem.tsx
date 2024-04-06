5bc2659d9b8f44b23f3285bac2087d88dc3141c5
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-06 13:02:41 -0400

    Handled the navigation to event details clicking on button or in image

--- app/components/EventItem.tsx
+++ app/components/EventItem.tsx
@@ -1,24 +1,31 @@
 import Button from "~/components/Button";
 import SearchOutlined from "~/assets/SearchOutlined";
 import { Database } from "../../database.types";
+import { Link, useNavigate } from "@remix-run/react";
 
 function EventItem({
   item,
-}: {
+}: Readonly<{
   item: Database["public"]["Tables"]["events"]["Row"] & {
     tickets: { count: number }[];
     categories: { id: number; name: string };
     users: { id: number; name: string };
   };
-}) {
+}>) {
+  const navigate = useNavigate();
+  const viewDetails = () => {
+    navigate(`/events/${item.id}`);
+  };
   return (
     <div className="flex flex-col border border-gray-300 rounded-lg shadow-md hover:shadow p-3">
       <div className="-m-3 mb-0">
-        <img
-          src={item.img_url ?? ""}
-          alt="event_img"
-          className="w-full h-[300px] rounded-lg rounded-b-[0] object-cover hover:brightness-105"
-        />
+        <Link to={`/events/${item.id}`}>
+          <img
+            src={item.img_url ?? ""}
+            alt="event_img"
+            className="w-full h-[300px] rounded-lg rounded-b-[0] object-cover hover:brightness-105"
+          />
+        </Link>
       </div>
       <h3 className="text-lg font-bold mt-3">{item.name}</h3>
       <div className="mt-7 flex-1 grid grid-cols-1 items-end">
@@ -28,6 +35,7 @@ function EventItem({
           style="secondary"
           postIcon={<SearchOutlined style={{ marginLeft: "7px" }} />}
           className="w-[80%] mx-auto"
+          onClick={viewDetails}
         />
       </div>
     </div>
