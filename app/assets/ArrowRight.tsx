17657590475aa69e29e403641acd5a8584bcc61d
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-12 15:48:15 -0400

    Asset component for arrow right icon

---
+++ app/assets/ArrowRight.tsx
@@ -0,0 +1,24 @@
+function ArrowRight(props: any) {
+  return (
+    <svg
+      className="fill-current"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M19 12H5m14 0-4 4m4-4-4-4"
+      />
+    </svg>
+  );
+}
+
+export default ArrowRight;
