5d36e3e521fca3690b86becfacfffeb02fd97535
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-12 23:06:17 -0400

    Implemented the search text in name and description

--- app/routes/_content.events._index/route.tsx
+++ app/routes/_content.events._index/route.tsx
@@ -3,23 +3,28 @@ import { useLoaderData } from "@remix-run/react";
 import createDBClient from "~/utils/supabase/server";
 import EventItem from "~/components/EventItem";
 
-export async function loader(args: LoaderFunctionArgs) {
-  const dbClient = createDBClient();
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const searchParams = url.searchParams;
+  const query = searchParams.get("query");
 
-  const {
-    data: events,
-    error,
-    count,
-  } = await dbClient
+  const dbClient = createDBClient();
+  const dbInstance = dbClient
     .from("events")
     .select("*, categories(id, name), users(id, name), tickets(count)");
+
+  if (query) {
+    dbInstance.textSearch("name_description", `${query}`);
+  }
+
+  const { data: events, error, count } = await dbInstance;
   return json({ events, error, count });
 }
 
 export default function Events() {
   const { events, count, error } = useLoaderData<typeof loader>();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-5 justify-items-center">
       {events?.map((item) => (
         // @ts-expect-error Insufficient type covered by supabase client
         <EventItem item={item} key={item.id} />
