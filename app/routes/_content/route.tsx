61a9800edf607ccbbdd1a8fe99ef363b3e2e2c8f
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-12 22:31:37 -0400

    Capture the search query in content layout

--- app/routes/_content/route.tsx
+++ app/routes/_content/route.tsx
@@ -1,10 +1,19 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import Header from "~/components/Header";
+import { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const query = url.searchParams.get("query");
+
+  return { query };
+};
 
 function ContentLayout() {
+  const { query } = useLoaderData<typeof loader>();
   return (
     <>
-      <Header />
+      <Header query={query} />
       <div className="w-full mt-5" />
       <div className="max-w-screen-2xl mx-auto px-2.5 lg:px-5">
         <Outlet />
