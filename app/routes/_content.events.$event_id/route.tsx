e077c481a3ef4de1c7e054d6217bed314626c9af
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-15 09:46:53 -0400

    Adjusted the display of full event description page
    -- Displayed the category name

--- app/routes/_content.events.$event_id/route.tsx
+++ app/routes/_content.events.$event_id/route.tsx
@@ -1,6 +1,7 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import createDBClient from "~/utils/supabase/server";
 import { useLoaderData } from "@remix-run/react";
+import SvgTag from "~/assets/SvgTag";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const dbClient = createDBClient();
@@ -20,11 +21,15 @@ function ViewEvent() {
   const { event } = useLoaderData<typeof loader>();
   return (
     <section className="">
-      <div className="gap-16 items-start lg:grid lg:grid-cols-2">
+      <div className="gap-5 md:gap-16 items-start grid grid-cols-1 md:grid-cols-2">
         <div className="font-light sm:text-lg">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold">
+          <h2 className="mb-2 text-xl lg:text-2xl tracking-tight font-bold">
             {event?.name}
           </h2>
+          <span className="flex gap-1 items-center font-light rounded-lg py-1.5 px-3 bg-secondary-100 w-fit mb-4 text-sm">
+            <SvgTag />
+            {event?.categories?.name}
+          </span>
           <p className="mb-4">{event?.description}</p>
           <p>
             We are strategists, designers and developers. Innovators and problem
