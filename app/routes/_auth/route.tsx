9fa56a77f71a2f7ea661da737b888863eeabb047
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-15 21:23:06 -0400

    Make the blur effect a bit lighter

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "@remix-run/react";
 function Auth() {
   return (
     <div className="bg-[url('/images/pablo-heimplatz-ZODcBkEohk8-unsplash.jpg')] w-screen h-screen bg-cover">
-      <div className="w-full h-full flex flex-col justify-center items-center backdrop-blur-lg gap-12">
+      <div className="w-full h-full flex flex-col justify-center items-center backdrop-blur-md gap-12">
         <Outlet />
       </div>
     </div>
