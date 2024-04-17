47cfba3d74a5b9727e568332ed32cde07a079a43
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-17 16:22:35 -0400

    Route for getting categories given a query

---
+++ app/routes/_content.categories/route.tsx
@@ -0,0 +1,10 @@
+import { ActionFunctionArgs } from "@remix-run/node";
+import createDBClient from "~/utils/supabase/server";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const query = formData.get("category_name") ?? "";
+  const dbClient = createDBClient({ request });
+
+  return dbClient.from("categories").select().ilike("name", `%${query}%`);
+}
