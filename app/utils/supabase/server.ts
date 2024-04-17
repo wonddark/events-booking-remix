1e8255f54fc52a741860066be76f202e3e659017
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-17 14:53:58 -0400

    createClient utility now uses the server version

--- app/utils/supabase/server.ts
+++ app/utils/supabase/server.ts
@@ -1,10 +1,26 @@
-import { createClient } from "@supabase/supabase-js";
+import { createServerClient, parse, serialize } from "@supabase/ssr";
 import { Database } from "../../../database.types";
 
-function createDBClient() {
-  return createClient<Database>(
+function createDBClient({ request }: { request: Request }) {
+  const cookies = parse(request.headers.get("Cookie") ?? "");
+  const headers = new Headers();
+
+  return createServerClient<Database>(
     process.env.DATABASE_URL!,
-    process.env.DATABASE_ADMIN_KEY!
+    process.env.DATABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(key) {
+          return cookies[key];
+        },
+        set(key, value, options) {
+          headers.append("Set-Cookie", serialize(key, value, options));
+        },
+        remove(key, options) {
+          headers.append("Set-Cookie", serialize(key, "", options));
+        },
+      },
+    }
   );
 }
 
