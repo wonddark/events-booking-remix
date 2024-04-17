0e436dd080f1637ca5c900e49003a08741014a0c
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-17 15:56:47 -0400

    Optimized the home page load by separating the statistic counters to...
    ...a new component with route loaders

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,46 +1,16 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link } from "@remix-run/react";
 import EventSVG from "~/assets/EventSVG";
-import createDBClient from "~/utils/supabase/server";
-import dayjs from "dayjs";
-import ArrowRight from "~/assets/ArrowRight";
 import Button from "../components/Button";
 import { useEffect, useState } from "react";
-import { LoaderFunctionArgs } from "@remix-run/node";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const dbClient = createDBClient({ request });
-  const today = dayjs().format("YYYY-MM-DD HH:mm:ss.sss");
-  const { data: recent } = await dbClient
-    .from("events")
-    .select("count")
-    .gt(
-      "published_at",
-      dayjs().subtract(72, "hours").format("YYYY-MM-DD HH:mm:ss.sss")
-    );
-  const { data: current } = await dbClient
-    .from("events")
-    .select("count")
-    .lt("start_date", today)
-    .gt("end_date", today);
-  const { data: soon } = await dbClient
-    .from("events")
-    .select("count")
-    .gt("start_date", today);
-
-  return {
-    recent: (recent?.[0] as unknown as { count: number }).count,
-    current: (current?.[0] as unknown as { count: number }).count,
-    soon: (soon?.[0] as unknown as { count: number }).count,
-  };
-}
+import HomeStatics from "~/components/HomeStatics";
 
 export default function Index() {
-  const { recent, current, soon } = useLoaderData<typeof loader>();
   const [loggedIn, setLoggedIn] = useState(false);
   useEffect(() => {
     const isLoggedIn = Boolean(localStorage.getItem("session"));
     setLoggedIn(isLoggedIn);
   }, []);
+
   return (
     <>
       <header className="bg-gradient-to-br from-[#c47b05ff] to-[#ffb933ff]">
@@ -132,47 +102,7 @@ export default function Index() {
         </div>
       </header>
 
-      <section className="max-w-screen-2xl mx-auto px-2.5 lg:px-5 py-5 flex flex-col md:flex-row gap-5 justify-around items-center flex-wrap">
-        <div className="text-center">
-          <div className="mb-3 md:mt-7">
-            <span className="text-5xl md:text-8xl text-primary-800">
-              {current ?? 0}
-            </span>
-          </div>
-          <Link
-            to="/events"
-            className="text-xl md:text-3xl text-primary-800 font-light flex items-center justify-center hover:brightness-125"
-          >
-            Running now <ArrowRight />
-          </Link>
-        </div>
-        <div className="text-center">
-          <div className="mb-3 md:mt-7">
-            <span className="text-5xl md:text-8xl text-primary-800">
-              {soon ?? 0}
-            </span>
-          </div>
-          <Link
-            to="/events"
-            className="text-xl md:text-3xl text-primary-800 font-light flex items-center justify-center hover:brightness-125"
-          >
-            Starting soon <ArrowRight />
-          </Link>
-        </div>
-        <div className="text-center">
-          <div className="mb-3 md:mt-7">
-            <span className="text-5xl md:text-8xl text-primary-800">
-              {recent ?? 0}
-            </span>
-          </div>
-          <Link
-            to="/events"
-            className="text-xl md:text-3xl text-primary-800 font-light flex items-center justify-center hover:brightness-125"
-          >
-            Recently posted <ArrowRight />
-          </Link>
-        </div>
-      </section>
+      <HomeStatics />
     </>
   );
 }
