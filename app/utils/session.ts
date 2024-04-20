19be6cd1be9a64243aa4c984d33451461a8505cc
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-20 16:36:31 -0400

    Utility function to refresh a session in supabase and set the authorization header in the client

--- app/utils/session.ts
+++ app/utils/session.ts
@@ -1,7 +1,27 @@
 import { getSession } from "~/sessions";
+import { SupabaseClient } from "@supabase/supabase-js";
 
 function getSessionFromCookie(request: Request) {
   return getSession(request.headers.get("Cookie"));
 }
 
-export { getSessionFromCookie };
+async function setAuthorization(request: Request, dbClient: SupabaseClient) {
+  const session = await getSessionFromCookie(request);
+
+  const { data, error } = await dbClient.auth.setSession({
+    access_token: session.get("access_token")!,
+    refresh_token: session.get("refresh_token")!,
+  });
+
+  if (data.session) {
+    dbClient.functions.setAuth(data.session?.access_token);
+  }
+
+  session.set("user_id", `${data.session?.user?.id}`);
+  session.set("access_token", `${data.session?.access_token}`);
+  session.set("refresh_token", `${data.session?.refresh_token}`);
+
+  return { session, error };
+}
+
+export { getSessionFromCookie, setAuthorization };
