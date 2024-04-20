69506db1ac1b1a929deaa5069be38cde8d2c4d18
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-19 21:27:53 -0400

    Added new assets for icons

---
+++ app/assets/Close.tsx
@@ -0,0 +1,21 @@
+function Close() {
+  return (
+    <svg
+      className="text-current"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fillRule="evenodd"
+        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default Close;
---
+++ app/assets/Floppy.tsx
@@ -0,0 +1,21 @@
+function Floppy() {
+  return (
+    <svg
+      className="text-current"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fillRule="evenodd"
+        d="M5 3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V7.414A2 2 0 0 0 20.414 6L18 3.586A2 2 0 0 0 16.586 3H5Zm10 11a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7V5h8v2a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default Floppy;
