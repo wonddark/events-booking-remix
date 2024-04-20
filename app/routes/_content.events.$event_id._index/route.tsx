d22fb3f1341ceec63c747c404cbb37346493b8bb
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-19 21:34:10 -0400

    Created an index for route /events/{eventID} to allow the creation of nested routes

---
+++ app/routes/_content.events.$event_id._index/route.tsx
@@ -0,0 +1,98 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import createDBClient from "~/utils/supabase/server";
+import { UIMatch, useLoaderData } from "@remix-run/react";
+import SvgTag from "~/assets/SvgTag";
+import BreadcrumbsLink from "~/components/BreadcrumbsLink";
+import BreadcrumbsPlain from "~/components/BreadcrumbsPlain";
+import { getSessionFromCookie } from "~/utils/session";
+import Button from "~/components/Button";
+import TrashBin from "~/assets/TrashBin";
+import Pen from "~/assets/Pen";
+
+// noinspection JSUnusedGlobalSymbols
+export const handle = {
+  breadcrumbs: (match: UIMatch, currentPath: boolean) => {
+    const name = (match.data as { event: { name: string } }).event.name;
+    if (currentPath) {
+      return <BreadcrumbsPlain key={match.id} name={name} />;
+    } else {
+      return (
+        <BreadcrumbsLink key={match.id} name={name} uri={match.pathname} />
+      );
+    }
+  },
+};
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+  const session = await getSessionFromCookie(request);
+  const dbClient = await createDBClient({ request });
+  const {
+    data: event,
+    error,
+    status,
+    statusText,
+  } = await dbClient
+    .from("events")
+    .select("*, categories(id, name), tickets(id, user_id), event_owner(*)")
+    .eq("id", params.event_id!);
+  return {
+    event: event?.[0],
+    error,
+    status,
+    statusText,
+    owner: session.get("user_id") === event?.[0].user_id,
+  };
+}
+
+function ViewEvent() {
+  const { event, owner } = useLoaderData<typeof loader>();
+  return (
+    <section className="">
+      <div className="gap-5 md:gap-16 items-start grid grid-cols-1 md:grid-cols-2">
+        <div className="font-light sm:text-lg">
+          <h2 className="mb-2 text-xl lg:text-2xl tracking-tight font-bold">
+            {event?.name}
+          </h2>
+          {owner && (
+            <div className="flex gap-1.5 -mt-2 mb-2">
+              <Button
+                label="Edit"
+                type="button"
+                style="secondary"
+                className="text-sm !py-1 !px-2"
+                preIcon={<Pen />}
+                asLink
+                href={`/events/${event?.id}/edit`}
+              />
+              <Button
+                label="Delete"
+                type="button"
+                style="danger"
+                className="text-sm !py-1 !px-2"
+                preIcon={<TrashBin />}
+              />
+            </div>
+          )}
+          <span className="flex gap-1 items-center font-light rounded-lg py-1.5 px-3 bg-secondary-100 w-fit mb-4 text-sm">
+            <SvgTag />
+            {event?.categories?.name}
+          </span>
+          <p className="mb-4">{event?.description}</p>
+          <p>
+            We are strategists, designers and developers. Innovators and problem
+            solvers. Small enough to be simple and quick.
+          </p>
+        </div>
+        <div className="">
+          <img
+            className="w-full rounded-lg"
+            src={event?.img_url ?? ""}
+            alt={event?.name ?? "event"}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ViewEvent;
--- app/routes/_content.events.$event_id/route.tsx
+++ app/routes/_content.events.$event_id/route.tsx
@@ -1,67 +1,7 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import createDBClient from "~/utils/supabase/server";
-import { UIMatch, useLoaderData } from "@remix-run/react";
-import SvgTag from "~/assets/SvgTag";
-import BreadcrumbsLink from "~/components/BreadcrumbsLink";
-import BreadcrumbsPlain from "~/components/BreadcrumbsPlain";
+import { Outlet } from "@remix-run/react";
 
-// noinspection JSUnusedGlobalSymbols
-export const handle = {
-  breadcrumbs: (match: UIMatch, currentPath: boolean) => {
-    const name = (match.data as { event: { name: string } }).event.name;
-    if (currentPath) {
-      return <BreadcrumbsPlain key={match.id} name={name} />;
-    } else {
-      return (
-        <BreadcrumbsLink key={match.id} name={name} uri={match.pathname} />
-      );
-    }
-  },
-};
-
-export async function loader({ params, request }: LoaderFunctionArgs) {
-  const dbClient = await createDBClient({ request });
-  const {
-    data: event,
-    error,
-    status,
-    statusText,
-  } = await dbClient
-    .from("events")
-    .select("*, categories(id, name), tickets(id, user_id), event_owner(*)")
-    .eq("id", params.event_id!);
-  return { event: event?.[0], error, status, statusText };
+function EventLayout() {
+  return <Outlet />;
 }
 
-function ViewEvent() {
-  const { event } = useLoaderData<typeof loader>();
-  return (
-    <section className="">
-      <div className="gap-5 md:gap-16 items-start grid grid-cols-1 md:grid-cols-2">
-        <div className="font-light sm:text-lg">
-          <h2 className="mb-2 text-xl lg:text-2xl tracking-tight font-bold">
-            {event?.name}
-          </h2>
-          <span className="flex gap-1 items-center font-light rounded-lg py-1.5 px-3 bg-secondary-100 w-fit mb-4 text-sm">
-            <SvgTag />
-            {event?.categories?.name}
-          </span>
-          <p className="mb-4">{event?.description}</p>
-          <p>
-            We are strategists, designers and developers. Innovators and problem
-            solvers. Small enough to be simple and quick.
-          </p>
-        </div>
-        <div className="">
-          <img
-            className="w-full rounded-lg"
-            src={event?.img_url ?? ""}
-            alt={event?.name ?? "event"}
-          />
-        </div>
-      </div>
-    </section>
-  );
-}
-
-export default ViewEvent;
+export default EventLayout;
