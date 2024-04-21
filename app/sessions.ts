b02479227c95df226e7afb84fb63f873599e7ebd
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-20 20:24:23 -0400

    Added new keys to SessionFlashData and set cookies to last 30 days

--- app/sessions.ts
+++ app/sessions.ts
@@ -8,11 +8,17 @@ type SessionData = {
 
 type SessionFlashData = {
   error: string;
+  success: string;
+  message: string;
+  warning: string;
 };
 
 const { getSession, commitSession, destroySession } =
   createCookieSessionStorage<SessionData, SessionFlashData>({
-    cookie: createCookie("__session", { secrets: ["234abc*"] }),
+    cookie: createCookie("__session", {
+      secrets: ["234abc*"],
+      maxAge: 2592000,
+    }),
   });
 
 export { getSession, commitSession, destroySession };
