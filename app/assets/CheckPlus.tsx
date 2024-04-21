bd20f9a32faa35c988b49310a92263ede06d39f4
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-20 20:19:24 -0400

    Icon for button 'Book a sit'

---
+++ app/assets/CheckPlus.tsx
@@ -0,0 +1,23 @@
+function CheckPlus() {
+  return (
+    <svg
+      className="text-current"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 21a9 9 0 1 1 0-18c1.052 0 2.062.18 3 .512M7 9.577l3.923 3.923 8.5-8.5M17 14v6m-3-3h6"
+      />
+    </svg>
+  );
+}
+
+export default CheckPlus;
