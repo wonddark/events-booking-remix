77ec97b5ae53b60c4d80903b125b4ffc62214dbe
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-20 20:24:53 -0400

    Fixed some issues with setAuthorization util

--- app/utils/session.ts
+++ app/utils/session.ts
@@ -1,4 +1,4 @@
-import { getSession } from "~/sessions";
+import { destroySession, getSession } from "~/sessions";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 function getSessionFromCookie(request: Request) {
@@ -8,18 +8,25 @@ function getSessionFromCookie(request: Request) {
 async function setAuthorization(request: Request, dbClient: SupabaseClient) {
   const session = await getSessionFromCookie(request);
 
-  const { data, error } = await dbClient.auth.setSession({
-    access_token: session.get("access_token")!,
-    refresh_token: session.get("refresh_token")!,
-  });
+  let error = null;
 
-  if (data.session) {
-    dbClient.functions.setAuth(data.session?.access_token);
+  if (session.get("access_token") && session.get("refresh_token")) {
+    const { data, error: sessionError } = await dbClient.auth.setSession({
+      access_token: session.get("access_token")!,
+      refresh_token: session.get("refresh_token")!,
+    });
+
+    error = sessionError;
+
+    if (data.session) {
+      dbClient.functions.setAuth(data.session?.access_token);
+      session.set("user_id", data.session.user.id);
+      session.set("access_token", data.session.access_token);
+      session.set("refresh_token", data.session.refresh_token);
+    }
+  } else {
+    await destroySession(session);
   }
-
-  session.set("user_id", `${data.session?.user?.id}`);
-  session.set("access_token", `${data.session?.access_token}`);
-  session.set("refresh_token", `${data.session?.refresh_token}`);
 
   return { session, error };
 }
