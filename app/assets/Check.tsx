31715a7e6c418b3a9092c188569b7b6ac05c8a09
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-25 00:31:41 -0400

    Implemented the tickets registration inside a modal

---
+++ app/assets/Check.tsx
@@ -0,0 +1,23 @@
+function Check() {
+  return (
+    <svg
+      className="text-current"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M5 11.917 9.724 16.5 19 7.5"
+      />
+    </svg>
+  );
+}
+
+export default Check;
--- app/components/EventItem.tsx
+++ app/components/EventItem.tsx
@@ -10,6 +10,9 @@ import {
   useRef,
   useState,
 } from "react";
+import { Modal } from "antd";
+import Close from "~/assets/Close";
+import Check from "~/assets/Check";
 
 type Props = Readonly<{
   item: Pick<
@@ -56,75 +59,85 @@ function EventItem({ item, userId }: Props) {
   const avaterRef = useRef<HTMLImageElement>(null);
 
   return (
-    <div className="flex flex-col border border-gray-300 rounded-lg shadow-md hover:shadow p-3 w-full md:w-[280px]">
-      <div className="-m-3 mb-0">
-        <Link to={`/events/${item.id}`}>
-          <img
-            src={
-              item.img_url
-                ? item.img_url
-                : "/images/event_image_placeholder.jpg"
-            }
-            onError={() =>
-              (imgRef.current!.src = "/images/event_image_placeholder.jpg")
-            }
-            alt="event_img"
-            className="w-full h-[240px] rounded-lg rounded-b-[0] object-cover hover:brightness-105"
-            loading="lazy"
-            ref={imgRef}
-          />
+    <>
+      <div className="flex flex-col border border-gray-300 rounded-lg shadow-md hover:shadow p-3 w-full md:w-[280px]">
+        <div className="-m-3 mb-0">
+          <Link to={`/events/${item.id}`}>
+            <img
+              src={
+                item.img_url
+                  ? item.img_url
+                  : "/images/event_image_placeholder.jpg"
+              }
+              onError={() =>
+                (imgRef.current!.src = "/images/event_image_placeholder.jpg")
+              }
+              alt="event_img"
+              className="w-full h-[240px] rounded-lg rounded-b-[0] object-cover hover:brightness-105"
+              loading="lazy"
+              ref={imgRef}
+            />
+          </Link>
+        </div>
+        <Link to={`/events/${item.id}`} className="font-bold mt-3">
+          {item.name}
         </Link>
-      </div>
-      <Link to={`/events/${item.id}`} className="font-bold mt-3">
-        {item.name}
-      </Link>
-      {item.event_owner && (
-        <Link
-          to={`/profiles/${item.event_owner.display_name}`}
-          className="flex items-center justify-start gap-1 mt-3"
-        >
-          <img
-            src={
-              item.event_owner.avatar
-                ? item.event_owner.avatar
-                : "/images/user_avatar_placeholder.jpeg"
-            }
-            onError={() =>
-              (avaterRef.current!.src = "/images/user_avatar_placeholder.jpeg")
-            }
-            ref={avaterRef}
-            alt={`${item.event_owner.display_name} avatar`}
-            className="rounded-full w-5 h-5 object-cover"
-          />
-          {item.event_owner.display_name}
-        </Link>
-      )}
-      <div className="mt-7 flex-1 flex justify-start items-end">
-        <Button
-          label="Details"
-          type="button"
-          style="secondary"
-          postIcon={<SearchOutlined />}
-          onClick={viewDetails}
-          className="w-3/5 mr-0.5"
-        />
-        {userId && item.event_owner?.user_id !== userId && (
+        {item.event_owner && (
+          <Link
+            to={`/profiles/${item.event_owner.display_name}`}
+            className="flex items-center justify-start gap-1 mt-3"
+          >
+            <img
+              src={
+                item.event_owner.avatar
+                  ? item.event_owner.avatar
+                  : "/images/user_avatar_placeholder.jpeg"
+              }
+              onError={() =>
+                (avaterRef.current!.src =
+                  "/images/user_avatar_placeholder.jpeg")
+              }
+              ref={avaterRef}
+              alt={`${item.event_owner.display_name} avatar`}
+              className="rounded-full w-5 h-5 object-cover"
+            />
+            {item.event_owner.display_name}
+          </Link>
+        )}
+        <div className="mt-7 flex-1 flex justify-start items-end">
           <Button
-            label="Book a sit"
+            label="Details"
             type="button"
-            style="primary"
-            postIcon={<CheckPlus />}
-            className="w-2/5 ml-0.5"
-            onClick={toggleTicketsForm}
+            style="secondary"
+            postIcon={<SearchOutlined />}
+            onClick={viewDetails}
+            className="w-3/5 mr-0.5"
           />
-        )}
-        {userId && item.event_owner?.user_id === userId && (
-          <span className="w-2/5 ml-0.5 py-1.5 px-3.5 border border-transparent text-right">
-            {item.tickets_count}
-          </span>
-        )}
+          {userId && item.event_owner?.user_id !== userId && (
+            <Button
+              label="Book a sit"
+              type="button"
+              style="primary"
+              postIcon={<CheckPlus />}
+              className="w-2/5 ml-0.5"
+              onClick={toggleTicketsForm}
+            />
+          )}
+          {userId && item.event_owner?.user_id === userId && (
+            <span className="w-2/5 ml-0.5 py-1.5 px-3.5 border border-transparent text-right">
+              {item.tickets_count}
+            </span>
+          )}
+        </div>
       </div>
-      {showTicketsForm && (
+      <Modal
+        open={showTicketsForm}
+        onCancel={toggleTicketsForm}
+        footer={null}
+        okText={null}
+        cancelText={null}
+        title="Register your sits"
+      >
         <registerTicket.Form
           method="post"
           action={`/events/${item.id}/tickets/register`}
@@ -136,16 +149,27 @@ function EventItem({ item, userId }: Props) {
             min={1}
             max={item.max_attendees - item.tickets_count}
             name="tickets_count"
+            className="w-full p-2 rounded-md mb-5"
           />
-          <Button
-            label="Ok"
-            type="submit"
-            loading={registerTicket.state === "submitting"}
-          />
-          <Button label="Cancel" type="button" onClick={cancelOp} />
+          <div className="flex justify-end items-center gap-2.5">
+            <Button
+              label="Cancel"
+              type="button"
+              onClick={cancelOp}
+              style="secondary"
+              preIcon={<Close />}
+            />
+            <Button
+              label="Submit"
+              type="submit"
+              loading={registerTicket.state === "submitting"}
+              style="primary"
+              preIcon={<Check />}
+            />
+          </div>
         </registerTicket.Form>
-      )}
-    </div>
+      </Modal>
+    </>
   );
 }
 
