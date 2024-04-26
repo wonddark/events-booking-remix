e7691e79001eb4a8dea2ab899b5afcdb8c0825ea
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-26 17:31:00 -0400

    Refactored the routes events/create and events/.../edit to use the EventForm component

--- app/routes/_content.events.$event_id.edit/route.tsx
+++ app/routes/_content.events.$event_id.edit/route.tsx
@@ -6,24 +6,19 @@ import {
 } from "@remix-run/node";
 import createDBClient from "~/utils/supabase/server";
 import {
-  Form,
   UIMatch,
   useActionData,
   useLoaderData,
   useNavigate,
-  useNavigation,
 } from "@remix-run/react";
 import BreadcrumbsLink from "~/components/BreadcrumbsLink";
 import BreadcrumbsPlain from "~/components/BreadcrumbsPlain";
-import Button from "~/components/Button";
 import { Database } from "../../../database.types";
-import CategorySelector from "~/components/CategorySelector";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import dayjs from "dayjs";
-import Floppy from "~/assets/Floppy";
-import Close from "~/assets/Close";
 import { commitSession, destroySession } from "~/sessions";
 import { setAuthorization } from "~/utils/session";
+import EventForm from "~/components/EventForm";
 
 // noinspection JSUnusedGlobalSymbols
 export const handle = {
@@ -60,7 +55,7 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
     statusText,
   } = await dbClient
     .from("events")
-    .select("*, categories(id, name), tickets(count)")
+    .select("*, categories(id, name)")
     .eq("id", params.event_id!);
 
   if (authorization.session.get("user_id") !== event?.[0].user_id) {
@@ -128,10 +123,6 @@ export async function action({ request, params }: ActionFunctionArgs) {
 function EditEvent() {
   const { event } = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
-  const inputCategoryRef = useRef<HTMLInputElement>(null);
-
-  const navigation = useNavigation();
-  const isSubmitting = navigation.formAction === `/events/${event?.id}/edit`;
 
   const navigate = useNavigate();
 
@@ -141,125 +132,7 @@ function EditEvent() {
     }
   }, [actionData]);
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-      <Form
-        className="flex flex-col items-center gap-3"
-        method="POST"
-        id="event-edit-form"
-      >
-        <div className="w-full">
-          <label htmlFor="event_name" className="text-primary-950">
-            Name{" "}
-            <input
-              type="text"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              placeholder="Event name"
-              id="event_name"
-              name="name"
-              defaultValue={event.name}
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_description" className="text-primary-950">
-            Description{" "}
-            <textarea
-              className={`rounded-lg py-1.5 px-3.5 h-40 w-full`}
-              placeholder="Write here a meaninful description for your new event..."
-              id="event_description"
-              name="description"
-              defaultValue={event.description}
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_attendees" className="text-primary-950">
-            Max attendees{" "}
-            <input
-              type="number"
-              min={50}
-              max={1_000_000}
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              placeholder="50"
-              id="event_attendees"
-              name="max_attendees"
-              defaultValue={event.max_attendees}
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_img" className="text-primary-950">
-            Image url{" "}
-            <input
-              type="url"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              placeholder="https://someservice.com/image.jpg"
-              id="event_img"
-              name="img_url"
-              defaultValue={event.img_url}
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_start_date" className="text-primary-950">
-            Starting at{" "}
-            <input
-              type="datetime-local"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              id="event_start_date"
-              name="start_date"
-              defaultValue={dayjs(event.start_date).format("YYYY-MM-DD HH:mm")}
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_end_date" className="text-primary-950">
-            Ending at{" "}
-            <input
-              type="datetime-local"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              id="event_end_date"
-              name="end_date"
-              defaultValue={dayjs(event.end_date).format("YYYY-MM-DD HH:mm")}
-            />
-          </label>
-        </div>
-
-        <input
-          type="text"
-          name="category_id"
-          hidden
-          ref={inputCategoryRef}
-          defaultValue={event.category_id}
-        />
-      </Form>
-
-      <div>
-        <CategorySelector
-          inputRef={inputCategoryRef}
-          defaultValue={event.categories.name}
-        />
-        <div className="text-right mt-3">
-          <Button
-            label="Cancel"
-            type="button"
-            style="light"
-            className="mr-2"
-            preIcon={<Close />}
-          />
-          <Button
-            label="Save event"
-            type="submit"
-            style="secondary"
-            loading={isSubmitting}
-            form="event-edit-form"
-            preIcon={<Floppy />}
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <EventForm event={event} />;
 }
 
 export default EditEvent;
--- app/routes/_content.events.create/route.tsx
+++ app/routes/_content.events.create/route.tsx
@@ -4,15 +4,14 @@ import {
   LoaderFunctionArgs,
   redirect,
 } from "@remix-run/node";
-import { Form, UIMatch, useActionData, useNavigation } from "@remix-run/react";
+import { UIMatch } from "@remix-run/react";
 import createDBClient from "~/utils/supabase/server";
 import { Database } from "../../../database.types";
-import Button from "~/components/Button";
-import { useRef } from "react";
 import { setAuthorization } from "~/utils/session";
 import BreadcrumbsPlain from "~/components/BreadcrumbsPlain";
-import CategorySelector from "~/components/CategorySelector";
 import { commitSession, destroySession } from "~/sessions";
+import EventForm from "~/components/EventForm";
+import dayjs from "dayjs";
 
 // noinspection JSUnusedGlobalSymbols
 export const handle = {
@@ -58,9 +57,13 @@ export async function action({ request }: ActionFunctionArgs) {
     name: formData.get("name") as string,
     description: formData.get("description") as string,
     img_url: formData.get("img_url") as string,
-    end_date: formData.get("end_date") as string,
+    end_date: dayjs(formData.get("end_date") as string).format(
+      "YYYY-MM-DD HH:mm:ss.sss"
+    ),
     max_attendees: Number(formData.get("max_attendees")),
-    start_date: formData.get("start_date") as string,
+    start_date: dayjs(formData.get("start_date") as string).format(
+      "YYYY-MM-DD HH:mm:ss.sss"
+    ),
   };
 
   const {
@@ -85,98 +88,7 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 function CreateEvent() {
-  const actionData = useActionData<typeof action>();
-  const navigation = useNavigation();
-  const isSubmitting = navigation.formAction === "/events/create";
-  const inputCategoryRef = useRef<HTMLInputElement>(null);
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-      <Form className="flex flex-col items-center gap-3" method="POST">
-        <div className="w-full">
-          <label htmlFor="event_name" className="text-primary-950">
-            Name{" "}
-            <input
-              type="text"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              placeholder="Event name"
-              id="event_name"
-              name="name"
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_description" className="text-primary-950">
-            Description{" "}
-            <textarea
-              className={`rounded-lg py-1.5 px-3.5 h-40 w-full`}
-              placeholder="Write here a meaninful description for your new event..."
-              id="event_description"
-              name="description"
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_attendees" className="text-primary-950">
-            Max attendees{" "}
-            <input
-              type="number"
-              min={50}
-              max={1_000_000}
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              placeholder="50"
-              id="event_attendees"
-              name="max_attendees"
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_img" className="text-primary-950">
-            Image url{" "}
-            <input
-              type="url"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              placeholder="https://someservice.com/image.jpg"
-              id="event_img"
-              name="img_url"
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_start_date" className="text-primary-950">
-            Starting at{" "}
-            <input
-              type="datetime-local"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              id="event_start_date"
-              name="start_date"
-            />
-          </label>
-        </div>
-        <div className="w-full">
-          <label htmlFor="event_end_date" className="text-primary-950">
-            Ending at{" "}
-            <input
-              type="datetime-local"
-              className={`rounded-lg py-1.5 px-3.5 w-full`}
-              id="event_end_date"
-              name="end_date"
-            />
-          </label>
-        </div>
-
-        <input type="text" name="category_id" hidden ref={inputCategoryRef} />
-
-        <Button
-          label="Save event"
-          type="submit"
-          style="primary"
-          loading={isSubmitting}
-        />
-      </Form>
-
-      <CategorySelector inputRef={inputCategoryRef} />
-    </div>
-  );
+  return <EventForm event={null} />;
 }
 
 export default CreateEvent;
