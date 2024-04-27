006c678a738449edc9b02d4142300f4c7c6f1756
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-27 13:38:38 -0400

    A reusable component to render in events lists

---
+++ app/components/EventsGrid.tsx
@@ -0,0 +1,27 @@
+import EventItem from "~/components/EventItem";
+import { EventElement } from "~/types/events";
+import NoResults from "~/components/NoResults";
+
+function EventsGrid({
+  events,
+  userId,
+}: Readonly<{
+  events: EventElement[];
+  userId: string | undefined;
+}>) {
+  return (
+    <>
+      {events.length > 0 ? (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-5 justify-items-center">
+          {events?.map((item) => (
+            <EventItem item={item} key={item.id} userId={userId} />
+          ))}
+        </div>
+      ) : (
+        <NoResults />
+      )}
+    </>
+  );
+}
+
+export default EventsGrid;
