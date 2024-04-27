0d02356c02e7adf84b830042e0a1d491d0ea010c
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-27 13:21:44 -0400

    Exported type EventElement for reuse in lists

--- app/types/events.ts
+++ app/types/events.ts
@@ -1,3 +1,5 @@
+import { Database } from "../../database.types";
+
 export type Event = {
   id: number;
   name: string;
@@ -13,4 +15,21 @@ export type Event = {
 export type EventsResponse = {
   events: Event[];
   total: number;
+};
+
+export type EventElement = Pick<
+  Database["public"]["Tables"]["events"]["Row"],
+  | "id"
+  | "img_url"
+  | "name"
+  | "tickets_count"
+  | "max_attendees"
+  | "start_date"
+  | "end_date"
+> & {
+  categories: Database["public"]["Tables"]["categories"]["Row"] | null;
+  event_owner: Pick<
+    Database["public"]["Views"]["event_owner"]["Row"],
+    "user_id" | "avatar" | "display_name"
+  > | null;
 };
