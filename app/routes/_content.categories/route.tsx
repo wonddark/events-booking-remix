36df185a075cc895c01039b10e9aa4f1bb50b654
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-29 17:49:20 -0400

    Added breadcrumbs item for categories page

--- app/routes/_content.categories/route.tsx
+++ app/routes/_content.categories/route.tsx
@@ -1,5 +1,22 @@
 import { ActionFunctionArgs } from "@remix-run/node";
 import createDBClient from "~/utils/supabase/server";
+import { UIMatch } from "@remix-run/react";
+import BreadcrumbsLink from "~/components/BreadcrumbsLink";
+import BreadcrumbsPlain from "~/components/BreadcrumbsPlain";
+
+// noinspection JSUnusedGlobalSymbols
+export const handle = {
+  breadcrumbs: (match: UIMatch, currentPath: boolean) => {
+    const name = "Categories";
+    if (currentPath) {
+      return <BreadcrumbsPlain key={match.id} name={name} />;
+    } else {
+      return (
+        <BreadcrumbsLink key={match.id} name={name} uri={match.pathname} />
+      );
+    }
+  },
+};
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
