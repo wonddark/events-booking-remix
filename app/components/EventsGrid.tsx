faaba3b582d75dc5411e31b44b238fa6363bedcd
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-28 22:36:13 -0400

    Finished the EventsGrid component with infinite scroll
    -- We needed to implement our own infinite scroll logic as a third
     package didn't seem to work properly

--- app/components/EventsGrid.tsx
+++ app/components/EventsGrid.tsx
@@ -1,25 +1,62 @@
 import EventItem from "~/components/EventItem";
 import { EventElement } from "~/types/events";
 import NoResults from "~/components/NoResults";
+import { useEffect, useState } from "react";
+import { useFetcher, useSearchParams } from "@remix-run/react";
+import { loader } from "~/routes/_content.events._index/route";
+import InfiniteScroller from "~/components/InfiniteScroller";
 
-function EventsGrid({
-  events,
-  userId,
-}: Readonly<{
-  events: EventElement[];
-  userId: string | undefined;
-}>) {
+interface EventsGridProps {
+  categoryId?: string;
+}
+
+function EventsGrid({ categoryId }: Readonly<EventsGridProps>) {
+  const fetcher = useFetcher<typeof loader>();
+  const [items, setItems] = useState<EventElement[]>([]);
+  const [page, setPage] = useState(0);
+  const [searchParams] = useSearchParams();
+  if (categoryId) {
+    searchParams.set("category_id", categoryId);
+  }
+
+  useEffect(() => {
+    fetcher.load(`/events?index&${searchParams}`);
+  }, []);
+
+  useEffect(() => {
+    if (!fetcher.data || fetcher.state === "loading") {
+      return;
+    }
+
+    if (fetcher.data) {
+      const newItems = fetcher.data.events;
+      setItems((prevAssets) => [...prevAssets, ...newItems]);
+      setPage(page + 1);
+    }
+  }, [fetcher.data]);
+
   return (
     <>
-      {events.length > 0 ? (
+      <InfiniteScroller
+        loadNext={() => {
+          if (fetcher.state !== "loading") {
+            const query = `/events?index&page=${page + 1}&${searchParams}`;
+            fetcher.load(query);
+          }
+        }}
+        loading={fetcher.state === "loading"}
+      >
         <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] gap-5 justify-items-center">
-          {events?.map((item) => (
-            <EventItem event={item} key={item.id} userId={userId} />
+          {items.map((item) => (
+            <EventItem
+              event={item}
+              key={item.id}
+              userId={fetcher.data?.userId}
+            />
           ))}
         </div>
-      ) : (
-        <NoResults />
-      )}
+      </InfiniteScroller>
+      {fetcher.state === "idle" && items.length === 0 && <NoResults />}
     </>
   );
 }
