5bc389fa5740515074b436f7439256faef7f5ffc
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-04-29 16:44:22 -0400

    Adjusted the height for logout button

--- app/routes/_content/Header/LogoutButton.tsx
+++ app/routes/_content/Header/LogoutButton.tsx
@@ -4,12 +4,13 @@ import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function LogoutButton() {
-  //
   const logout = useFetcher<typeof action>();
-  //
   return (
     <logout.Form method="post" action="/logout">
-      <button type="submit" className="flex items-center justify-start gap-2">
+      <button
+        type="submit"
+        className="flex items-center justify-start gap-2 h-8"
+      >
         <FontAwesomeIcon icon={faArrowRightFromBracket} />
         <span>Logout</span>
       </button>
