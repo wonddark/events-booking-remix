6e77331d134ddbb7d29fff6b05f91849f1524190
Osmanys Fuentes-Lombá <ossmanys@gmail.com>
2024-05-30 11:12:30 -0400

    Adjusted the home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,6 +3,8 @@ import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
 import createDBClient from "~/utils/supabase/server";
 import { setAuthorization } from "~/utils/session";
 import { commitSession, destroySession } from "~/sessions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 // noinspection JSUnusedGlobalSymbols
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -28,11 +30,11 @@ export default function Index() {
   return (
     <>
       <div className="bg-[url('/images/valiant-made-UrzN-8K1PCE-unsplash.webp')] min-h-screen bg-cover bg-no-repeat bg-center flex flex-col justify-between px-6 md:px-14">
-        <header className="flex flex-col md:flex-row justify-between items-center gap-3 py-6 w-full max-w-screen-2xl mx-auto">
+        <header className="flex justify-between items-center gap-3 py-4 md:py-6 w-full max-w-screen-2xl mx-auto">
           <span className="bg-gradient-to-b from-amber-700 to-amber-300 bg-clip-text text-transparent text-2xl md:text-3xl font-semibold">
             EventsBooking
           </span>
-          <nav className="flex-1 flex justify-end">
+          <nav className="flex-1 hidden md:flex justify-end">
             <ul className="flex items-center gap-1.5 uppercase font-medium tracking-wide">
               <li>
                 <a
@@ -52,15 +54,20 @@ export default function Index() {
               </li>
             </ul>
           </nav>
+          <button className="text-white md:hidden w-fit text-xl p-1">
+            <FontAwesomeIcon icon={faBars} />
+          </button>
         </header>
         <div className="flex flex-col gap-8 md:gap-12 mb-[30%] md:mb-[10%] xl:mb-[5%] w-full max-w-screen-2xl mx-auto">
-          <p className="text-3xl md:text-4xl xl:text-6xl font-semibold text-white leading-[1.25] w-[60%] min-w-[300px] max-w-[870px]">
-            When shared with friends every moment is a{" "}
-            <span className="text-amber-500">memorable adventure</span>
-          </p>
-          <p className="text-xl md:text-2xl xl:text-3xl font-medium leading-[1.15] tracking-wide">
-            We can help you keep your most precious memories alive
-          </p>
+          <div className="w-full max-w-[870px]">
+            <p className="text-3xl md:text-4xl xl:text-6xl font-semibold text-white leading-[1.25]">
+              When shared with friends every moment is a{" "}
+              <span className="text-amber-500">memorable adventure</span>
+            </p>
+            <p className="text-xl md:text-2xl font-medium leading-[1.15] tracking-wide text-white mt-2">
+              We can help you keep your most precious memories alive
+            </p>
+          </div>
           <a
             href="/events/create"
             className="text-lg py-1.5 px-3 bg-amber-500 w-fit font-semibold text-gray-800 rounded hover:brightness-110 hover:rounded-md"
